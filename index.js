028d79d8d1842565276dee1cc1d26e300ea076c2
Alan José Goulart <89042443+GoulartAJG@users.noreply.github.com>
2022-12-23 13:33:54 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+/*!
+ * Released under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function(num) {
+  if (typeof num === 'number') {
+    return num - num === 0;
+  }
+  if (typeof num === 'string' && num.trim() !== '') {
+    return Number.isFinite ? Number.isFinite(+num) : isFinite(+num);
+  }
+  return false;
+};
